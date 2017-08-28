ae76bbf151eccb23eaf593fdb4b9f52bb137be20
kacper <kacper.kowalski@appunite.com>
2017-08-28 10:46:24 +0200

    setup

---
+++ BudgetList/Budget.swift
@@ -0,0 +1,9 @@
+//
+//  Budget.swift
+//  BudgetList
+//
+//  Created by Kacper Kowalski on 28.08.2017.
+//  Copyright © 2017 Kacper Kowalski. All rights reserved.
+//
+
+import Foundation
---
+++ BudgetList/BudgetListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetListViewController.swift
+//  BudgetList
+//
+//  Created by Kacper Kowalski on 28.08.2017.
+//  Copyright © 2017 Kacper Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class BudgetListViewController: UITableViewController {
+    
+    
+    
+    
+    
+}
